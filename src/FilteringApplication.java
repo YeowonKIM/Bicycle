9f6498a5338c8fe43c45935f506c16775dcf6bdc
YeowonKIM <kkyoo152@gmail.com>
2024-12-18 05:00:35 +0100

    Measure execution time

---
+++ src/FilteringApplication.java
@@ -0,0 +1,9 @@
+import controller.FilteringController;
+import controller.SortingController;
+
+public class FilteringApplication {
+    public static void main(String[] args) {
+        FilteringController filteringController = new FilteringController();
+        filteringController.runFilteringNavigator();
+    }
+}
